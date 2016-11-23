fff6b459b663076915aa21c97227bbfc7d4d3e54
Tanja Hennis <tmhennis@gmail.com>
2016-11-23 15:29:39 +0100

    form element

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -10,13 +10,18 @@ constructor(props) {
 }
 
 onInputChange(event) {
-  console.log(event.target.value);
   this.setState({ term: event.target.value });
+}
+
+onFormSubmit(event) {
+  event.preventDefault();
+
+  // We need to go and fetch weather data
 }
 
   render() {
     return(
-      <form className="input-group">
+      <form onSubmit={this.onFormSubmit} className="input-group">
         <input
           placeholder="Get a five-day forecast in your favourite cities"
           className="form-control"
