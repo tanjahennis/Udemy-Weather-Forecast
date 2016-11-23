5f7158c78a5d4e91f63277c6b17e1b47a4bfd107
Tanja Hennis <tmhennis@gmail.com>
2016-11-23 16:10:53 +0100

    ajax request with axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = '0e93ae2329125c859e06159860f200f3';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`; //,us is limiting the search to US cities
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
